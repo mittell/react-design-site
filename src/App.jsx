0890ba4eb600d225fb29b5b820a35bfb38da4df6
Chris Mittell <mittell@hotmail.co.uk>
2022-09-08 17:36:55 +0900

    feat: added HomePage component and basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
-function App() {
-	return <div></div>;
-}
+import React from 'react';
+import GlobalStyle from './globalStyles';
+import HomePage from './pages/HomePage';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+const App = () => {
+	return (
+		<Router>
+			<GlobalStyle />
+			<Switch>
+				<Route path='/' exact component={HomePage} />
+			</Switch>
+		</Router>
+	);
+};
 
 export default App;
