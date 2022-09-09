8a8657fce988032706a9f0800a3d380876a2e0ef
Chris Mittell <mittell@hotmail.co.uk>
2022-09-09 15:57:14 +0900

    chore: removed data

--- src/data/NavbarData.js
+++ src/data/NavbarData.js
@@ -3,12 +3,12 @@ export const navbarData = [
 		to: 'about',
 		text: 'About',
 	},
-	{
-		to: 'clients',
-		text: 'Clients',
-	},
-	{
-		to: 'projects',
-		text: 'Projects',
-	},
+	// {
+	// 	to: 'clients',
+	// 	text: 'Clients',
+	// },
+	// {
+	// 	to: 'projects',
+	// 	text: 'Projects',
+	// },
 ];
