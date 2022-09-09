793f066e0c23b2ebeb0abdc10af85f1de15a5dea
Chris Mittell <mittell@hotmail.co.uk>
2022-09-09 15:41:55 +0900

    chore: added Features reference

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Features from '../components/Features/Features';
 import Hero from '../components/Hero/Hero';
 
 const HomePage = () => {
-	return <Hero />;
+	return (
+		<>
+			<Hero />
+			<Features />
+		</>
+	);
 };
 
 export default HomePage;
